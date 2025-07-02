2badc3719c5e6279dcbf43f7171405a6193673eb
JORGELBURGOS <jorgeleonburgos@gmail.com>
2025-07-02 10:54:32 -0300

    carga1

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let data = [];
+    let filtros = {
+        periodo: "",
+        clase: "Todas",
+        sucursal: "",
+        oficial: ""
+    };
+    // Código de carga y render similar al entregado antes...
+});
